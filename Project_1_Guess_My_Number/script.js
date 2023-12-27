9bed0030209dff87bea40908f9a9574180152be1
kgour <karangour@gmail.com>
2023-12-27 13:34:21 +0530

    updated

--- Project_1_Guess_My_Number/script.js
+++ Project_1_Guess_My_Number/script.js
@@ -1,33 +1,41 @@
 document.addEventListener('readystatechange', function () {
+  //let page load before js logic starts.
   if (document.readyState === 'complete') {
-    let score = 20;
-    let highscore = 0;
+    //once it's loaded..
 
     const randNum = () => {
       return Math.floor(Math.random() * 20) + 1;
     };
 
-    async function display(id, message) {
+    async function display(id, message, time) {
       document.querySelector(id).textContent = message;
-      return new Promise ((resolve) => setTimeout(resolve, 1000))
+      return new Promise(resolve => setTimeout(resolve, time * 1000));
     }
-  
 
-    async function rightOrWrong (n, c) {
+    async function rightOrWrong(n, c) {
       if (n === c) {
-        await display('.start-guessing', 'Correct!!')
+        await display('.start-guessing', 'Correct!!', 1);
         return true;
       } else {
-        await display('.start-guessing', 'Try again!!')
+        if (n < c) await display('.start-guessing', 'Too high!', 1);
+        if (n > c) await display('.start-guessing', 'Too low!', 1);
         return false;
       }
-    };
+    }
 
-    async function initializer () {
+    async function initializer() {
+      let gameEnded = false;
       let num = randNum();
       let choice = null;
       let s = 'Start guessing';
       let counter = 0;
+      let score = 20;
+
+      //document.querySelector('#number').textContent = num;
+
+      document.querySelector('.input #type').value = '';
+      document.querySelector('#score').textContent = 'SCORE: ' + score;
+      document.querySelector('.number #number').textContent = '?';
 
       let blink = setInterval(() => {
         switch (counter % 4) {
@@ -51,40 +59,108 @@ document.addEventListener('readystatechange', function () {
         .querySelector('#button-check')
         .addEventListener('click', async () => {
           clearInterval(blink);
-          document.querySelector('#number').textContent = num; //show 'my number'
-          choice = document.querySelector('.input #type').value;
-          const isCorrect = await rightOrWrong(num, choice)
-          if (isCorrect) {
-            if (highscore < score) {
-              highscore = score;
-              document.querySelector('#highscore').textContent =
-                'Highscore: ' + highscore;
-              await display('#score', 'SCORE: RECORD!!')
-              score = 20;
-              initializer();
-              };
+
+          choice = Number(document.querySelector('.input #type').value); //this has to be converted to a number since it's ANY input by user is ALWAYS a string by default
+
+          if (!choice) {
+            display('.start-guessing', 'No number!', 1);
+            counter = 0;
+            s = 'Start guessing';
+
+            if (!gameEnded) {
+              blink = setInterval(() => {
+                switch (counter % 4) {
+                  case 0:
+                    document.querySelector('.start-guessing').textContent = s;
+                    break;
+                  case 1:
+                    document.querySelector('.start-guessing').textContent =
+                      s + '.';
+                    break;
+                  case 2:
+                    document.querySelector('.start-guessing').textContent =
+                      s + '..';
+                    break;
+                  default:
+                    document.querySelector('.start-guessing').textContent =
+                      s + '...';
+                    break;
+                }
+                counter++;
+              }, 500);
             }
-          else {
-            if (score === 0) {
-              await (display('.start-guessing', 'Uh-oh! Please RESTART!'))
-            } else {
-              console.log(score);
-              score = score - 1;
-              await (display ('#score', 'SCORE: ' + score))
-              //document.querySelector('#score').textContent = 'SCORE: ' + score;
-              initializer();
-              
+          } else {
+            const isCorrect = await rightOrWrong(num, choice);
+            if (isCorrect) {
+              gameEnded = true;
+              //document.querySelector('#number').textContent = num; //show 'my number'
+              if (highscore < score) {
+                document.querySelector('#number').textContent = num; //show 'my number'
+                highscore = score;
+                document.querySelector('#highscore').textContent =
+                  'Highscore: ' + highscore;
+                score = 20;
+                display('#score', 'SCORE: RECORD!!', 1);
+                document.querySelector('.start-guessing').textContent =
+                  'Press RESTART!';
+              } else
+                document.querySelector('.start-guessing').textContent =
+                  'Press RESTART!';
+            } else if (!isCorrect) {
+              //document.querySelector('#number').textContent = num; //show 'my number'
+              if (score === 0) {
+                gameEnded = true;
+                document.querySelector('#number').textContent = num; //show 'my number'
+                document.querySelector('.start-guessing').textContent =
+                  'Uh-oh! Please RESTART!';
+              }
+              if (score > 0) {
+                //document.querySelector('#number').textContent = num; //show 'my number'
+                //console.log(score);
+                score = score - 1;
+                display('#score', 'SCORE: ' + score, 0.2);
+
+                counter = 0;
+                s = 'Start guessing';
+
+                if (!gameEnded) {
+                  blink = setInterval(() => {
+                    switch (counter % 4) {
+                      case 0:
+                        document.querySelector('.start-guessing').textContent =
+                          s;
+                        break;
+                      case 1:
+                        document.querySelector('.start-guessing').textContent =
+                          s + '.';
+                        break;
+                      case 2:
+                        document.querySelector('.start-guessing').textContent =
+                          s + '..';
+                        break;
+                      default:
+                        document.querySelector('.start-guessing').textContent =
+                          s + '...';
+                        break;
+                    }
+                    counter++;
+                  }, 500);
+                }
+              }
             }
           }
-          
         });
-    };
+    }
+
+    //let score = 20;
+    let highscore = 0;
 
     initializer();
     document
-    .querySelector('#restart')
-    .addEventListener('click', () => this.location.reload(true));
+      .querySelector('.top #restart')
+      .addEventListener('click', async () => {
+        console.log('inside restart button');
+        initializer();
+      });
   }
-
-  
 });
