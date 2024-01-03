880af1817f09841bc721eaf8559123083ab514a0
kgour <karangour@gmail.com>
2024-01-03 18:24:25 +0530

    modal project

--- Project_2_Modal_Window/06-Modal/final/script.js
+++ Project_2_Modal_Window/06-Modal/final/script.js
@@ -1,24 +1,30 @@
+'use strict';
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
+
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-let i = 0;
-while (i < btnsOpenModal.length) {
-  btnsOpenModal[i].addEventListener('click', openModal); //openModal with the () has to be called because it needs to execute AFTER click and not as soon as it's read by JS.
-  i++;
-}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', (e) => {
-    if ((e.key === 'Escape') && !(modal.classList.contains('hidden'))) closeModal();
-    
-})
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
